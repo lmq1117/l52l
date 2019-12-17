527566c5d67339a4450e9ef023ff00a2b4b8f004
limq <limq@cgnfs.com.cn>
2019-12-17 14:34:29 +0800

    Username

---
+++ app/Console/Commands/Username.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Foundation\Inspiring;
+
+class Username extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'username';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Display an inspiring quote';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        //$this->comment(PHP_EOL.Inspiring::quote().PHP_EOL);
+        //$this->line(PHP_EOL.Inspiring::quote().PHP_EOL);
+        $this->info(PHP_EOL."username".PHP_EOL);
+        //$this->question (PHP_EOL.Inspiring::quote().PHP_EOL);
+        //$this->error (PHP_EOL.Inspiring::quote().PHP_EOL);
+    }
+}
